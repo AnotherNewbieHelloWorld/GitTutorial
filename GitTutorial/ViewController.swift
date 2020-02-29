31ed3891d45ec0a2bcdadf9d4f39fe896cb6eebf
AnotherNewbieHelloWorld <khor.alena16@gmail.com>
2020-02-29 20:39:46 +0500

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Apple User on 29.02.2020.
+//  Copyright © 2020 Alena Khoroshilova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
